f9102220f469acc5805bea43d6763626ecda84c5
harrisonkay2112 <harryjkay@hotmail.com>
2025-03-22 17:11:53 -0400

    button functionality to WIP login.html
    Edited login.html and app.js to allow for forgot password button to redirect user to new page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-// Backend functionality goes here
+document.addEventListener("DOMContentLoaded", function () {
+  const btnForgotPass = document.getElementById('btnForgotPass');
+  const btnBackToLogin = document.getElementById('btnBackToLogin');
+  const frmLoginDiv = document.getElementById('frmLogin');
+  const resetPasswordDiv = document.getElementById('resetPasswordContent');
+
+ 
+btnForgotPass.addEventListener('click', () => {
+  frmLoginDiv.style.display = 'none';
+  resetPasswordDiv.style.display = 'flex';
+});
+
+btnBackToLogin.addEventListener('click', () => {
+  resetPasswordDiv.style.display = 'none';
+  frmLoginDiv.style.display = 'flex';
+});
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
