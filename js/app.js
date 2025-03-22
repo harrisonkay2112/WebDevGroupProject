2a87a327af0df0b3f2254d2e2cd486cc029f5718
dlneisz42 <dlneisz42@tntech.edu>
2025-03-22 02:08:22 -0500

    Basic button functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-// Backend functionality goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const btnStudent = document.getElementById("btnStudentRegister");
+    const btnInstructor = document.getElementById("btnInstructorRegister");
+  
+    const studentDiv = document.getElementById("studentRegistration");
+    const instructorDiv = document.getElementById("instructorRegistration");
+    const selectRegisterDiv = document.getElementById("frmRegister");
+  
+    btnStudent.addEventListener("click", function (event) {
+      event.preventDefault();
+      selectRegisterDiv.style.display = "none";
+      studentDiv.style.display = "block";
+      instructorDiv.style.display = "none";
+    });
+  
+    btnInstructor.addEventListener("click", function (event) {
+      event.preventDefault();
+      selectRegisterDiv.style.display = "none";
+      instructorDiv.style.display = "block";
+      studentDiv.style.display = "none";
+    });
+  });
+  
