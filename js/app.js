5115af449c4bc33207d43deaec916102eed1e3f1
harrisonkay2112 <harryjkay@hotmail.com>
2025-03-24 13:27:50 -0500

    Login Page buttons fully functional
    Set up all Buttons to successfully validate emails on both login and reset password forms, as well as successfully redirects user to registration page when chosen

--- js/app.js
+++ js/app.js
@@ -1,27 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
   const btnForgotPass = document.getElementById('btnForgotPass');
   const btnBackToLogin = document.getElementById('btnBackToLogin');
-  const btnToRegister = document.getElementById('btnSwitchRegister');
-
+  const btnSwitchRegister = document.getElementById('btnSwitchRegister');
+  const btnSendResetPassword = document.getElementById('btnResetEmail');
+  
+  const strLoginEmail = document.getElementById('loginEmail'); 
+  const strSendResetEmail = document.getElementById('resetEmail')
   const frmLoginDiv = document.getElementById('frmLogin');
-  const resetPasswordDiv = document.getElementById('resetPasswordContent');
-  const frmRegisterDiv = document.getElementById('RegisterContent')
+  const divResetPassword = document.getElementById('resetPasswordContent');
+  
 
+  // Login Button Email validation
+  btnLogin.addEventListener('click', () => {
+    const email = strLoginEmail.value.trim();
+    if (!validateEmail(email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+    //console.log(email);
+  });
+
+  // Reset Password send Email Button validation
+  btnSendResetPassword.addEventListener('click', () =>{
+    const email = strSendResetEmail.value.trim
+    if (!validateEmail(email)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+    //console.log(email);
+  });
  
+// Validate inputed string as an email, returns True or False
+  function validateEmail(email) {
+    // Simple email regex
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+
 //redirects user to forgot password form
 btnForgotPass.addEventListener('click', () => {
   frmLoginDiv.style.display = 'none';
-  resetPasswordDiv.style.display = 'flex';
+  divResetPassword.style.display = 'flex';
 });
 
-//sends user back to login screen from forgot password screen
+//redirects user back to login screen from forgot password screen
 btnBackToLogin.addEventListener('click', () => {
-  resetPasswordDiv.style.display = 'none';
+  divResetPassword.style.display = 'none';
   frmLoginDiv.style.display = 'flex';
 });
 
-btnToRegister.addEventListener('click', () => {
-  window.location.href = "/components/register.html";
+//Redirects user to Register Page
+btnSwitchRegister.addEventListener('click', () => {
+  window.location.href = "./register.html";
 
 });
 
