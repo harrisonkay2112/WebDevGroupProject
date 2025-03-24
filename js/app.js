dcd5dfcf13cc607d5ae902cc45e391fa9feceb33
harrisonkay2112 <harryjkay@hotmail.com>
2025-03-24 18:57:27 -0500

    WIP Login Page updated and completed
    Added Sweet Alerts and fixed button logic

--- js/app.js
+++ js/app.js
@@ -1,31 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //Variable Declaration
+  const btnLogin = document.getElementById('btnLogin');
   const btnForgotPass = document.getElementById('btnForgotPass');
   const btnBackToLogin = document.getElementById('btnBackToLogin');
   const btnSwitchRegister = document.getElementById('btnSwitchRegister');
   const btnSendResetPassword = document.getElementById('btnResetEmail');
   
-  const strLoginEmail = document.getElementById('loginEmail'); 
+
+  const strLoginEmail = document.getElementById('emailLogin'); 
   const strSendResetEmail = document.getElementById('resetEmail')
   const frmLoginDiv = document.getElementById('frmLogin');
   const divResetPassword = document.getElementById('resetPasswordContent');
   
 
-  // Login Button Email validation
-  btnLogin.addEventListener('click', () => {
+  // Login Button Email validation, if not valid, runs sweet alert error
+  btnLogin.addEventListener('click', (event) => {
+    event.preventDefault();
     const email = strLoginEmail.value.trim();
     if (!validateEmail(email)) {
-      alert("Please enter a valid email address.");
-      return;
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid Email',
+        text: 'Ooops, Please enter a valid email address.'
+      });
     }
+  
     //console.log(email);
   });
 
-  // Reset Password send Email Button validation
-  btnSendResetPassword.addEventListener('click', () =>{
-    const email = strSendResetEmail.value.trim
+  // Reset Password send Email Button validation, if not valid, runs sweet alert error, if valid, notifies email was sent
+  btnSendResetPassword.addEventListener('click', (event) =>{
+    event.preventDefault();
+    const email = strSendResetEmail.value.trim();
     if (!validateEmail(email)) {
-      alert("Please enter a valid email address.");
-      return;
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid Email',
+        text: 'Ooops, Please enter a valid email address.'
+      });
+    }
+    else{
+      Swal.fire({
+        icon: 'success',
+        title: 'Email Sent!',
+        text: 'Check your inbox for the reset link.'
+      });
+
     }
     //console.log(email);
   });
@@ -39,13 +59,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 //redirects user to forgot password form
-btnForgotPass.addEventListener('click', () => {
+btnForgotPass.addEventListener('click', (event) => {
+  event.preventDefault();
   frmLoginDiv.style.display = 'none';
   divResetPassword.style.display = 'flex';
 });
 
 //redirects user back to login screen from forgot password screen
-btnBackToLogin.addEventListener('click', () => {
+btnBackToLogin.addEventListener('click', (event) => {
+  event.preventDefault();
   divResetPassword.style.display = 'none';
   frmLoginDiv.style.display = 'flex';
 });
