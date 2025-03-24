50dd992fdc5a091d29507f79f43d033f134b3d4b
harrisonkay2112 <harryjkay@hotmail.com>
2025-03-24 12:19:05 -0500

    Added Partial Button Redirect Functionality to login.html
    WIP Button functionality to have login.html redirect to register.html, to finish later tonight

--- js/app.js
+++ js/app.js
@@ -1,24 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   const btnForgotPass = document.getElementById('btnForgotPass');
   const btnBackToLogin = document.getElementById('btnBackToLogin');
+  const btnToRegister = document.getElementById('btnSwitchRegister');
+
   const frmLoginDiv = document.getElementById('frmLogin');
   const resetPasswordDiv = document.getElementById('resetPasswordContent');
+  const frmRegisterDiv = document.getElementById('RegisterContent')
 
  
+//redirects user to forgot password form
 btnForgotPass.addEventListener('click', () => {
   frmLoginDiv.style.display = 'none';
   resetPasswordDiv.style.display = 'flex';
 });
 
+//sends user back to login screen from forgot password screen
 btnBackToLogin.addEventListener('click', () => {
   resetPasswordDiv.style.display = 'none';
   frmLoginDiv.style.display = 'flex';
 });
 
-
-
+btnToRegister.addEventListener('click', () => {
+  window.location.href = "/components/register.html";
 
 });
+
+});
+
+
 
 
 
